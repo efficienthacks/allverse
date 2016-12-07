be6e92af20030cf6f75e28f3197815c926b829be
Clay Smith <csmith@corp.flagshipcredit.com>
2016-12-07 13:48:21 -0500

    user no longer requires email address

--- Models/AccountViewModels/RegisterViewModel.cs
+++ Models/AccountViewModels/RegisterViewModel.cs
@@ -9,6 +9,9 @@ namespace WebApplication.Models.AccountViewModels
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "User Name")]
+        public string UserName {get;set;}
+
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
